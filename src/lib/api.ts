3e4402446200177fc809994d450ca91795472626
gemtunde <gemtunde@gmail.com>
2025-05-01 16:20:17 +0100

    integrate analytics api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import API from "./axios-client";
 import {
   AllWorkspaceResponseType,
+  AnalyticsResponseType,
   CreateWorkspaceResponseType,
   CreateWorkspaceType,
   CurrentUserResponseType,
@@ -73,7 +74,12 @@ export const deleteWorkspaceMutationFn = async (
   return response.data;
 };
 
-export const getWorkspaceAnalyticsQueryFn = async () => {};
+export const getWorkspaceAnalyticsQueryFn = async (
+  workspaceId: string
+): Promise<AnalyticsResponseType> => {
+  const response = await API.get(`/workspace/analytics/${workspaceId}`);
+  return response.data;
+};
 
 export const changeWorkspaceMemberRoleMutationFn = async () => {};
 
